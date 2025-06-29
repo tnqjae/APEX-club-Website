2e80cd1be00c81d1d9163fa63c212e51a5abe5b6
SteffenLee <tnqjae@naver.com>
2025-06-29 12:10:21 +0900

    add tab icon

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,3 @@
 import AppHeader from './components/AppHeader.vue'
 import HeroImage from './components/MainPageImg.vue'
 </script>
-
-<style>
-html, body {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: 'Segoe UI', sans-serif;
-}
-</style>
