96b3f2d452be85d21b9d21ade02d7c48145b36a9
SteffenLee <tnqjae@naver.com>
2025-06-30 14:19:50 +0900

    Add Main Introduction

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <AppHeader />
   <HeroImage />
+  <MainPageIntroduction />
 </template>
 
 <script setup>
 import AppHeader from './components/AppHeader.vue'
 import HeroImage from './components/MainPageImg.vue'
+import MainPageIntroduction from './components/MainPageIntroduction.vue'
 </script>
---
+++ src/components/MainPageIntroduction.vue
@@ -0,0 +1,10 @@
+<template>
+    <section class="bg-[#0f131c] min-h-screen">
+        <h1 class="text-white text-4xl font-bold text-center pt-10">
+            Welecome to APEX!
+        </h1>
+        <h3 class="text-white text-xl font-semibold text-center mt-4">
+            It's a club for students who are interested in Security / A.I. / Forensics
+        </h3>
+    </section>
+</template>
