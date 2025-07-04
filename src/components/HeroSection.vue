a08f31b7aa1c667f6c029f7ce206e699085f2922
SteffenLee <tnqjae@naver.com>
2025-07-04 15:53:45 +0900

    Add HeroSection Img and Hero Component

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,22 @@
+<!-- HeroSection.vue -->
+<template>
+  <section>
+    <div class="mx-auto">
+      <img
+        :src="imageSrc"
+        :alt="altText"
+        class="w-full h-auto block object-cover max-h-[400px] sm:max-h-[600px]"
+      />
+    </div>
+  </section>
+</template>
+
+<script setup>
+defineProps({
+  imageSrc: String,
+  altText: {
+    type: String,
+    default: 'Hero Image'
+  }
+})
+</script>
