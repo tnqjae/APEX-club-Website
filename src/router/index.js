f33cec66570bcd77d7eaece957f230929f5f3e7e
SteffenLee <tnqjae@naver.com>
2025-07-05 22:08:51 +0900

    setting router

--- src/router/index.js
+++ src/router/index.js
@@ -2,18 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Home       from '@/views/Home.vue'
 import Professor  from '@/views/Professor.vue'
-//import Team       from '@/views/Team.vue'
-//import Student    from '@/views/Student.vue'
-//import Project    from '@/views/Project.vue'
-//import Gallery    from '@/views/Gallery.vue'
+import Team       from '@/views/Team.vue'
+import Student    from '@/views/Student.vue'
+import Project    from '@/views/Project.vue'
+import Gallery    from '@/views/Gallery.vue'
 
 const routes = [
   { path: '/',           component: Home       },
   { path: '/professor',  component: Professor  },
-  //{ path: '/team',       component: Team       },
-  //{ path: '/student',    component: Student    },
-  //{ path: '/project',    component: Project    },
-  //{ path: '/gallery',    component: Gallery    },
+  { path: '/team',       component: Team       },
+  { path: '/student',    component: Student    },
+  { path: '/project',    component: Project    },
+  { path: '/gallery',    component: Gallery    },
 ]
 
 export default createRouter({
