b930f3aa59229e576f56eeb33f0166992e7b53b1
SteffenLee <tnqjae@naver.com>
2025-07-21 21:11:10 +0900

    chore: add JS obfuscation plugin for build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import obfuscator from 'rollup-plugin-obfuscator'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,4 +18,26 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+   build: {
+    minify: 'terser', // 기본 압축도 포함
+    rollupOptions: {
+      plugins: [
+        obfuscator({
+          compact: true,
+          controlFlowFlattening: true,
+          controlFlowFlatteningThreshold: 1,
+          numbersToExpressions: true,
+          simplify: true,
+          shuffleStringArray: true,
+          splitStrings: true,
+          splitStringsChunkLength: 5,
+          stringArray: true,
+          stringArrayEncoding: ['rc4'],
+          stringArrayThreshold: 1,
+          selfDefending: true,
+          disableConsoleOutput: true,
+        })
+      ]
+    }
+  }
 })
