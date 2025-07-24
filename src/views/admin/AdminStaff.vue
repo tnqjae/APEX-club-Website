a51162c544dd12d22c9a93ea0a29688e6dacf355
SteffenLee <tnqjae@naver.com>
2025-07-25 00:40:08 +0900

    Add Authorization

--- src/views/admin/AdminStaff.vue
+++ src/views/admin/AdminStaff.vue
@@ -6,5 +6,38 @@
 </template>
 
 <script setup>
-// 향후 로직 작성
+import { SearchSlash } from 'lucide-vue-next'
+import { onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const token = sessionStorage.getItem('adminToken')
+const isAuthorized = ref(false)
+
+onMounted(async () => {
+  if (!token) {
+    alert("관리자 권한이 없습니다. 로그인 해주세요.")
+    return
+  }
+
+  try {
+    const res = await fetch(import.meta.env.VITE_ADVERIFY_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      }
+    })
+    const result = await res.json()
+
+    if (res.ok && result.success) {
+      isAuthorized.value = true
+    } else {
+      throw new Error('Invalid token')
+    }
+  } catch (err) {
+    SearchSlash.removeItem('adminToken')
+    router.push('/adminMain')
+  }
+})
 </script>
