fdcf19a557ba3e245acf61b852d044478541f72c
SteffenLee <tnqjae@naver.com>
2025-07-24 17:52:22 +0900

    Init

---
+++ src/views/admin/AdminStaff.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="text-white p-6">
+    <h1 class="text-2xl font-bold">Admin Staff Page</h1>
+    <p>추후 구현 예정입니다.</p>
+  </div>
+</template>
+
+<script setup>
+// 향후 로직 작성
+</script>
